e17cf57a8350f14625e757e841b242689a016bf3
13456922906 <394568365@qq.com>
2017-09-28 15:56:37 +0800

    链接设置界面

--- app/src/main/java/info/btsland/app/MainActivity.java
+++ app/src/main/java/info/btsland/app/MainActivity.java
@@ -3,32 +3,49 @@ package info.btsland.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private ImageView ivNavUser ;
+    private TextView tvNavSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //User
         ivNavUser=(ImageView)findViewById(R.id.iv_nav_user);
         ivNavUser.setOnClickListener(new ivNavUserOnClick());
-
+        //Setting
+        tvNavSet = (TextView)findViewById(R.id.tv_nav_set);
+        tvNavSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
     class ivNavUserOnClick implements View.OnClickListener{
         @Override
         public void onClick(View view) {
-            Intent intent=new Intent(MainActivity.this,UserActivity.class);
-            MainActivity.this.startActivity(intent);
+                Intent intent=new Intent(MainActivity.this,UserActivity.class);
+                startActivity(intent);
         }
     }
+
+
+
     protected void show(){
         System.out.print("测试");
         System.out.print("测试2");
         System.out.print("测试3");
         System.out.print("测试4");
     }
-
 }

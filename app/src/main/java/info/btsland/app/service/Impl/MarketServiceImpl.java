144c2f398dba29d62138896625eccb13a03d5192
xjh0415 <1120280598@qq.com>
2017-09-30 13:51:57 +0800

    完成行情页的交易对美化

--- app/src/main/java/info/btsland/app/service/Impl/MarketServiceImpl.java
+++ app/src/main/java/info/btsland/app/service/Impl/MarketServiceImpl.java
@@ -35,22 +35,22 @@ public class MarketServiceImpl implements MarketService {
 /**
  * BTS测试数据
  */
-        List<Market> listBTS=new ArrayList<Market>();
-        listBTS.add(new Market("BTC","BTS","0.00002109","0.00002105","0.00002121","20.734",3.11f));
-        listBTS.add(new Market("bitCNY","BTS","1.76806","1.77905","1.77937","6.31M",-9.02f));
-        listBTS.add(new Market("bitUSD","BTS","11.93180","11.80174","11.92885","346.71k",-10.71f));
-        listBTS.add(new Market("ETH","BTS","3504.27338","3536.06789","3577.92045","186.82k",-4.95f));
-        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
-        listBTS.add(new Market("BTC","BTS","0.00002109","0.00002105","0.00002121","20.734",3.11f));
-        listBTS.add(new Market("bitCNY","BTS","1.76806","1.77905","1.77937","6.31M",-9.02f));
-        listBTS.add(new Market("bitUSD","BTS","11.93180","11.80174","11.92885","346.71k",-10.71f));
-        listBTS.add(new Market("ETH","BTS","3504.27338","3536.06789","3577.92045","186.82k",-4.95f));
-        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
-        listBTS.add(new Market("BTC","BTS","0.00002109","0.00002105","0.00002121","20.734",3.11f));
-        listBTS.add(new Market("bitCNY","BTS","1.76806","1.77905","1.77937","6.31M",-9.02f));
-        listBTS.add(new Market("bitUSD","BTS","11.93180","11.80174","11.92885","346.71k",-10.71f));
-        listBTS.add(new Market("ETH","BTS","3504.27338","3536.06789","3577.92045","186.82k",-4.95f));
-        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
+        List<Market> listBTC=new ArrayList<Market>();
+        listBTC.add(new Market("BTS","BTC","0.00002109","0.00002105","0.00002121","7.808",-0.88f));
+        listBTC.add(new Market("bitCNY","BTC","0.00003593","0.00001987","0.00002000","3.634",-4.72f));
+        listBTC.add(new Market("bitUSD","BTC","0.00024433","0.00024433","0.00024804","2.588",22.17f));
+        listBTC.add(new Market("ETH","BTC","0.7018957","0.06978142","0.07018208","0.147",-0.48f));
+        listBTC.add(new Market("IMIAO","BTC","1","1","1","1",0f));
+        listBTC.add(new Market("BTS","BTC","0.00002109","0.00002105","0.00002121","7.808",-0.88f));
+        listBTC.add(new Market("bitCNY","BTC","0.00003593","0.00001987","0.00002000","3.634",-4.72f));
+        listBTC.add(new Market("bitUSD","BTC","0.00024433","0.00024433","0.00024804","2.588",22.17f));
+        listBTC.add(new Market("ETH","BTC","0.7018957","0.06978142","0.07018208","0.147",-0.48f));
+        listBTC.add(new Market("IMIAO","BTC","1","1","1","1",0f));
+        listBTC.add(new Market("BTS","BTC","0.00002109","0.00002105","0.00002121","7.808",-0.88f));
+        listBTC.add(new Market("bitCNY","BTC","0.00003593","0.00001987","0.00002000","3.634",-4.72f));
+        listBTC.add(new Market("bitUSD","BTC","0.00024433","0.00024433","0.00024804","2.588",22.17f));
+        listBTC.add(new Market("ETH","BTC","0.7018957","0.06978142","0.07018208","0.147",-0.48f));
+        listBTC.add(new Market("IMIAO","BTC","1","1","1","1",0f));
         /**
          * bitCNY测试数据
          */
@@ -60,20 +60,50 @@ public class MarketServiceImpl implements MarketService {
         listbitCNY.add(new Market("bitUSD","bitCNY","6.80921","6.78063","6.80850","94.02k",1.76f));
         listbitCNY.add(new Market("ETH","bitCNY","2000.00000","1998.62926","2000.00000","113.84k",7.94f));
         listbitCNY.add(new Market("IMIAO","bitCNY","1","1","1","1",0f));
+        listbitCNY.add(new Market("BTC","bitCNY","27490.00171","26999.99919","27489.99802","156.95k",10.27f));
+        listbitCNY.add(new Market("BTS","bitCNY","0.576000","0.573000","0.576300","3.44M",13.18f));
+        listbitCNY.add(new Market("bitUSD","bitCNY","6.80921","6.78063","6.80850","94.02k",1.76f));
+        listbitCNY.add(new Market("ETH","bitCNY","2000.00000","1998.62926","2000.00000","113.84k",7.94f));
+        listbitCNY.add(new Market("IMIAO","bitCNY","1","1","1","1",0f));
+        listbitCNY.add(new Market("BTC","bitCNY","27490.00171","26999.99919","27489.99802","156.95k",10.27f));
+        listbitCNY.add(new Market("BTS","bitCNY","0.576000","0.573000","0.576300","3.44M",13.18f));
+        listbitCNY.add(new Market("bitUSD","bitCNY","6.80921","6.78063","6.80850","94.02k",1.76f));
+        listbitCNY.add(new Market("ETH","bitCNY","2000.00000","1998.62926","2000.00000","113.84k",7.94f));
+        listbitCNY.add(new Market("IMIAO","bitCNY","1","1","1","1",0f));
         /**
          * BTC测试数据
          */
 
-        List<Market> listBTC =new ArrayList<Market>();
-        listBTC.add(new Market("BTS","BTC","48167.88078","47508.33115","48167.56325","1.02M",-1.72f));
-        listBTC.add(new Market("bitCNY","BTC","1.70000","1.72554","1.74664","6.68M",-11.84f));
-        listBTC.add(new Market("bitUSD","BTC","11.78503","11.78503","11.90351","366.58k",-10.14f));
-        listBTC.add(new Market("ETH","BTC","3518.64900","3476.00237","3518.64867","176.46k",-4.28f));
-        listBTC.add(new Market("IMIAO","BTC","1","1","1","1",0f));
+        List<Market> listBTS =new ArrayList<Market>();
+        listBTS.add(new Market("BTC","BTS","48167.88078","47508.33115","48167.56325","1.02M",-1.72f));
+        listBTS.add(new Market("bitCNY","BTS","1.70000","1.72554","1.74664","6.68M",-11.84f));
+        listBTS.add(new Market("bitUSD","BTS","11.78503","11.78503","11.90351","366.58k",-10.14f));
+        listBTS.add(new Market("ETH","BTS","3518.64900","3476.00237","3518.64867","176.46k",-4.28f));
+        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
+        listBTS.add(new Market("BTC","BTS","48167.88078","47508.33115","48167.56325","1.02M",-1.72f));
+        listBTS.add(new Market("bitCNY","BTS","1.70000","1.72554","1.74664","6.68M",-11.84f));
+        listBTS.add(new Market("bitUSD","BTS","11.78503","11.78503","11.90351","366.58k",-10.14f));
+        listBTS.add(new Market("ETH","BTS","3518.64900","3476.00237","3518.64867","176.46k",-4.28f));
+        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
+        listBTS.add(new Market("BTC","BTS","48167.88078","47508.33115","48167.56325","1.02M",-1.72f));
+        listBTS.add(new Market("bitCNY","BTS","1.70000","1.72554","1.74664","6.68M",-11.84f));
+        listBTS.add(new Market("bitUSD","BTS","11.78503","11.78503","11.90351","366.58k",-10.14f));
+        listBTS.add(new Market("ETH","BTS","3518.64900","3476.00237","3518.64867","176.46k",-4.28f));
+        listBTS.add(new Market("IMIAO","BTS","1","1","1","1",0f));
         /**
          * bitUSD测试数据
          */
         List<Market> listbitUSD =new ArrayList<Market>();
+        listbitUSD.add(new Market("BTS","bitUSD","10.084100","0.084114","0.084848","29.80k",10.30f));
+        listbitUSD.add(new Market("bitCNY","bitUSD","0.14749","0.14600","0.14748","14.12k",-0.85f));
+        listbitUSD.add(new Market("BTC","bitUSD","4051.07188","4028.05566","4049.99978","15.52k",7.20f));
+        listbitUSD.add(new Market("ETH","bitUSD","301.16279","284.39281","301.44213","308.734k",50.58f));
+        listbitUSD.add(new Market("IMIAO","bitUSD","1","1","1","1",0f));
+        listbitUSD.add(new Market("BTS","bitUSD","10.084100","0.084114","0.084848","29.80k",10.30f));
+        listbitUSD.add(new Market("bitCNY","bitUSD","0.14749","0.14600","0.14748","14.12k",-0.85f));
+        listbitUSD.add(new Market("BTC","bitUSD","4051.07188","4028.05566","4049.99978","15.52k",7.20f));
+        listbitUSD.add(new Market("ETH","bitUSD","301.16279","284.39281","301.44213","308.734k",50.58f));
+        listbitUSD.add(new Market("IMIAO","bitUSD","1","1","1","1",0f));
         listbitUSD.add(new Market("BTS","bitUSD","10.084100","0.084114","0.084848","29.80k",10.30f));
         listbitUSD.add(new Market("bitCNY","bitUSD","0.14749","0.14600","0.14748","14.12k",-0.85f));
         listbitUSD.add(new Market("BTC","bitUSD","4051.07188","4028.05566","4049.99978","15.52k",7.20f));

38200932d0cc99b39a1168bc4dfd5ac6a4ac9c32
lw1000 <1578318880@qq.com>
2017-10-20 17:37:08 +0800

    创建用户设置类，及布局文件

---
+++ app/src/main/java/info/btsland/app/ui/activity/UsersetActivity.java
@@ -0,0 +1,99 @@
+package info.btsland.app.ui.activity;
+
+/**
+ * author：lw1000
+ * function：用户设置类
+ * 2017/10/20.
+ */
+
+
+public class UsersetActivity {
+
+
+    /**
+     * 用户资产
+     */
+    private String uAsset;
+
+
+    /**
+     * 换算值
+     */
+    private String scaledValue;
+
+
+
+    /**
+     * 供给占比
+     */
+    private String supplyShare;
+
+
+
+
+    public UsersetActivity(String uAsset, String scaledValue, String supplyShare) {
+        this.uAsset = uAsset;
+        this.scaledValue = scaledValue;
+        this.supplyShare = supplyShare;
+    }
+
+
+    public String getuAsset() {
+        return uAsset;
+    }
+
+
+    public void setuAsset(String uAsset) {
+        this.uAsset = uAsset;
+    }
+
+
+    public String getScaledValue() {
+        return scaledValue;
+    }
+
+
+    public void setScaledValue(String scaledValue) {
+        this.scaledValue = scaledValue;
+    }
+
+
+    public String getSupplyShare() {
+        return supplyShare;
+    }
+
+
+    public void setSupplyShare(String supplyShare) {
+        this.supplyShare = supplyShare;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

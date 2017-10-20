63d83516f7d284a008eefed8a203a352cb0cccd6
xjh0415 <1120280598@qq.com>
2017-10-20 17:39:36 +0800

    增加详细行情窗口

--- app/src/main/java/info/btsland/app/ui/activity/MarketDetailedActivity.java
+++ app/src/main/java/info/btsland/app/ui/activity/MarketDetailedActivity.java
@@ -1,12 +1,22 @@
 package info.btsland.app.ui.activity;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import info.btsland.app.Adapter.DetailedFragmentAdapter;
+import info.btsland.app.Adapter.ImagePageAdapter;
 import info.btsland.app.R;
 import info.btsland.app.model.Market;
+import info.btsland.app.ui.fragment.DetailedBuyAndSellFragment;
+import info.btsland.app.ui.fragment.DetailedHaveInHandFragment;
+import info.btsland.app.ui.fragment.DetailedKFragment;
 import info.btsland.app.ui.fragment.HeadFragment;
 
 public class MarketDetailedActivity extends AppCompatActivity {
@@ -25,7 +35,19 @@ public class MarketDetailedActivity extends AppCompatActivity {
      * 初始化
      */
     private void init(){
-
+        ViewPager viewPager= (ViewPager) findViewById(R.id.vp_detailed_page);
+        String[] titles={"详情","买/卖","进行中"};
+        List<Fragment> fragments=new ArrayList<Fragment>();
+        DetailedKFragment detailedKFragment=new DetailedKFragment();
+        DetailedBuyAndSellFragment detailedBuyAndSellFragment=new DetailedBuyAndSellFragment();
+        DetailedHaveInHandFragment detailedHaveInHandFragment=new DetailedHaveInHandFragment();
+        fragments.add(detailedKFragment);
+        fragments.add(detailedBuyAndSellFragment);
+        fragments.add(detailedHaveInHandFragment);
+        DetailedFragmentAdapter adapter=new DetailedFragmentAdapter(getSupportFragmentManager(),fragments,titles);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+        viewPager.addOnPageChangeListener(new OnPage());
     }
     /**
      * 装载顶部导航
@@ -38,5 +60,22 @@ public class MarketDetailedActivity extends AppCompatActivity {
             transaction.add(R.id.fra_detailed_head,headFragment);
         }
         transaction.commit();
+    }
+    class OnPage implements ViewPager.OnPageChangeListener{
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }

d93cc3ca24d09974f6d08df11897fe74888bd709
15759882500 <15759882500@163.com>
2017-10-25 16:42:12 +0800

    增加导航切换功能

--- app/src/main/java/info/btsland/app/ui/fragment/HomeFragment.java
+++ app/src/main/java/info/btsland/app/ui/fragment/HomeFragment.java
@@ -3,18 +3,25 @@ package info.btsland.app.ui.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.astuetz.PagerSlidingTabStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.btsland.app.Adapter.DetailedFragmentAdapter;
 import info.btsland.app.R;
 
 public class HomeFragment extends Fragment {
-
+    private String TAG="HomeFragment";
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,57 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_news, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: ");
+        init();
+    }
+
+    /**
+     * 初始化
+     */
+    private void init(){
+        Log.e(TAG, "init: ");
+        ViewPager viewPager1= (ViewPager) getActivity().findViewById(R.id.vp_detailed);
+        String[] titles={"最新资讯","热点资讯","国内资讯","国外资讯"};
+       List<Fragment> fragments=new ArrayList<Fragment>();
+        LatestNewsFragment latestNewsFragment=new LatestNewsFragment();
+        HotNewsFragment hotNewsFragment=new HotNewsFragment();
+        DomesticInformationFragment domesticInformationFragment=new DomesticInformationFragment();
+        ForeignInformationFragment foreignInformationFragment=new ForeignInformationFragment();
+        fragments.add(latestNewsFragment);
+        fragments.add(hotNewsFragment);
+        fragments.add(domesticInformationFragment);
+        fragments.add(foreignInformationFragment);
+        DetailedFragmentAdapter adapter=new DetailedFragmentAdapter(getChildFragmentManager(),fragments,titles);
+        PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) getActivity().findViewById(R.id.psts_detailed_title1);
+        viewPager1.setAdapter(adapter);
+        viewPager1.setCurrentItem(0);
+        tabStrip.setViewPager(viewPager1);
+        tabStrip.setOnPageChangeListener(new OnPage());
+    }
+
+    class OnPage implements ViewPager.OnPageChangeListener{
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
--- app/src/main/java/info/btsland/app/ui/fragment/LatestNewsFragment.java
+++ app/src/main/java/info/btsland/app/ui/fragment/LatestNewsFragment.java
@@ -1,23 +1,13 @@
 package info.btsland.app.ui.fragment;
 
-/**
- * Created by zyf on 2017/10/8.
- * 与fragment_newstitle.xml相对应
- * 根据碎片的生命周期,onAttach()方法会首先执行,因此在这里做了一些数据初始化的操作,比如调用 getNews()方法获取几
- * 条新闻数据,以及完成 NewsAdapter 的创建。然后在 onCreateView()方法中加载了news_title_frag 布局,
- * 并给新闻列表的 ListView 注册了点击事件。
- * 然后在 ListView 的点击事件里我们就可以判断,如果当前是单页模式,就启动一个新的活动去显示新闻内容
- */
-
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -29,7 +19,7 @@ import info.btsland.app.model.News;
 import info.btsland.app.ui.activity.NewsContentActivity;
 
 
-public class NewsTitleFragment extends Fragment implements OnItemClickListener {
+public class LatestNewsFragment extends Fragment implements AdapterView.OnItemClickListener {
     private ListView newsTitleListView;
     private List<News> newsList;
     private NewsAdapter adapter;
@@ -46,7 +36,7 @@ public class NewsTitleFragment extends Fragment implements OnItemClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //加载fragment_newstitle.xml 布局
-        View view = inflater.inflate(R.layout.fragment_newstitle, container, false);
+        View view = inflater.inflate(R.layout.fragment_latest_news, container, false);
         //得到ListView的实例
         newsTitleListView = (ListView) view.findViewById(R.id.news_title_list_view);
         //启动ListView的适配器，这样ListView就能与适配器的数据相关联了

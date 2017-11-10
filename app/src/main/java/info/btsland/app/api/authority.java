e07001754b5608c4a08f942ede126ec5d6a480fd
xjh0415 <1120280598@qq.com>
2017-11-10 11:20:44 +0800

    登录注册

--- app/src/main/java/info/btsland/app/api/authority.java
+++ app/src/main/java/info/btsland/app/api/authority.java
@@ -1,5 +1,6 @@
 package info.btsland.app.api;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,16 +13,28 @@ public class authority {
 
 //    public authority(int nWeightThreshold, types.public_key_type publicKeyType, int nWeightType) {
 //        weight_threshold = nWeightThreshold;
-//        key_auths.put(publicKeyType, nWeightType);
+//        //key_auths.put(publicKeyType, nWeightType);
 //    }
 
-//    public boolean is_public_key_type_exist(types.public_key_type publicKeyType) {
-//        return key_auths.containsKey(publicKeyType);
-//    }
-//
-//    public List<types.public_key_type> get_keys() {
-//        List<types.public_key_type> listKeyType = new ArrayList<>();
-//        listKeyType.addAll(key_auths.keySet());
-//        return listKeyType;
-//    }
+    public boolean is_public_key_type_exist(types.public_key_type publicKeyType) {
+        for(int i=0;i<key_auths.size();i++){
+            if(key_auths.get(i).get(0).equals(publicKeyType.toString())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<types.public_key_type> get_keys() {
+        List<types.public_key_type> listKeyType = new ArrayList<>();
+        for(int i=0;i<key_auths.get(0).size();i++){
+            String str=key_auths.get(0).get(i);
+            try {
+                listKeyType.add(new types.public_key_type(str));
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return listKeyType;
+    }
 }

f5c49b557d101b5c6619fb54645ad927e7e9318f
xjh0415 <1120280598@qq.com>
2017-11-24 18:23:13 +0800

    优化低内存手机运行时界面错乱

--- app/src/main/java/info/btsland/app/ui/activity/VersionInformationActivity.java
+++ app/src/main/java/info/btsland/app/ui/activity/VersionInformationActivity.java
@@ -1,8 +1,11 @@
 package info.btsland.app.ui.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import info.btsland.app.R;
 import info.btsland.app.ui.fragment.HeadFragment;
@@ -10,6 +13,7 @@ import info.btsland.app.ui.fragment.HeadFragment;
 public class VersionInformationActivity extends BaseActivity {
 
     private HeadFragment headFragment;
+    private TextView tvVersionName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,24 @@ public class VersionInformationActivity extends BaseActivity {
         setContentView(R.layout.activity_setting_edition);
         fillInHead();
         init();
+        fillIn();
+    }
+
+    private void fillIn() {
+        try {
+            PackageInfo info=getPackageManager().getPackageInfo(getPackageName(),0);
+            String verName=info.versionName;
+            tvVersionName.setText(verName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 初始化
      */
     private void init() {
+        tvVersionName=findViewById(R.id.tv_about_verName);
     }
 
     /**

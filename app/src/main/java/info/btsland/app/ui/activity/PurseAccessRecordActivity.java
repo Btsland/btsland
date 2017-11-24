bf47ff3c8af8343d231543b954f940e73f2e3c69
lw1000 <1578318880@qq.com>
2017-11-24 17:31:52 +0800

    修改一些bug

--- app/src/main/java/info/btsland/app/ui/activity/PurseAccessRecordActivity.java
+++ app/src/main/java/info/btsland/app/ui/activity/PurseAccessRecordActivity.java
@@ -1,7 +1,6 @@
 package info.btsland.app.ui.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
@@ -22,6 +21,9 @@ public class PurseAccessRecordActivity extends AppCompatActivity{
         fillInHead();
         init();
     }
+
+
+
 
     /**
      * 初始化

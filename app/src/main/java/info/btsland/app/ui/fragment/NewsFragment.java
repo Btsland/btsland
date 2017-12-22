913d59e31dd764b788a293514514c53842a12133
lw1000 <1578318880@qq.com>
2017-12-22 14:53:43 +0800

    修改新闻功能1

--- app/src/main/java/info/btsland/app/ui/fragment/NewsFragment.java
+++ app/src/main/java/info/btsland/app/ui/fragment/NewsFragment.java
@@ -35,7 +35,7 @@ public class NewsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private WebView webView;
-    private String url = "http://www.fastchain.info/";
+    private String url = "http://www.fastchain.info/index.php";
     private String Tag = "MQL";
 
 //    private OnFragmentInteractionListener mListener;
@@ -90,11 +90,11 @@ public class NewsFragment extends Fragment {
 
         webView.getSettings().setJavaScriptEnabled(true);
         // 设置可以支持缩放
-      //  webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setSupportZoom(true);
         // 设置出现缩放工具
-       // webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setBuiltInZoomControls(true);
         //扩大比例的缩放
-       // webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setUseWideViewPort(true);
         //自适应屏幕
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         webView.getSettings().setLoadWithOverviewMode(true);
@@ -107,10 +107,10 @@ public class NewsFragment extends Fragment {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 //该方法在Build.VERSION_CODES.LOLLIPOP以前有效，从Build.VERSION_CODES.LOLLIPOP起，建议使用shouldOverrideUrlLoading(WebView, WebResourceRequest)} instead
                 //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
-                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://www.fastchain.info/
 
-                if (url.toString().contains("www.fastchain.info")){
-                    view.loadUrl("http://www.fastchain.info/");
+                if (url.toString().contains("http://www.fastchain.info/?cat=14")){
+                    view.loadUrl("http://www.fastchain.info/index.php");
                     return true;
                 }
 
@@ -121,10 +121,10 @@ public class NewsFragment extends Fragment {
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
             {
                 //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
-                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://www.fastchain.info/
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    if (request.getUrl().toString().contains("www.fastchain.info")){
-                        view.loadUrl("http://www.fastchain.info/");
+                    if (request.getUrl().toString().contains("http://www.fastchain.info/?cat=14")){
+                        view.loadUrl("http://www.fastchain.info/index.php");
                         return true;
                     }
                 }
